7cd85c64bf61e7d800673d00f312e68b431d443c
Guilherme Cabral <guilhermecabral.ccb@gmail.com>
2025-01-26 10:50:49 -0300

    Update instruçãoSQL.sql

--- instruçãoSQL.sql
+++ instruçãoSQL.sql
@@ -1,17 +1,23 @@
-SELECT
-        CustomerID,
-        Title,
-        FirstName,
-        CompanyName,
-        Phone,
-        SalesPerson
-FROM
-        SalesLT.Customer
-WHERE
-        CompanyName IN (
-        'A Bike Store',
-		'A Cycle Shop',
-		'A Great Bicycle Company',
-		'A Typical Bike Shop',
-		'Action Bicycle Specialists'
-		)
+SELECT 
+    SOD.SalesOrderID,
+    SOD.ProductID,
+    SOD.OrderQty,
+    SOD.UnitPrice,
+    SOD.LineTotal,
+    SOH.OrderDate,
+    SOH.SalesOrderNumber,
+    SOH.TotalDue,
+    C.CustomerID,
+    C.FirstName,
+    C.LastName,
+    C.EmailAddress
+FROM 
+    SalesLT.SalesOrderDetail AS SOD
+INNER JOIN 
+    SalesLT.SalesOrderHeader AS SOH
+ON 
+    SOD.SalesOrderID = SOH.SalesOrderID
+INNER JOIN 
+    SalesLT.Customer AS C
+ON 
+    SOH.CustomerID = C.CustomerID;
